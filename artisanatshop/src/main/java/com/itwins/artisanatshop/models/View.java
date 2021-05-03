09af970a2111e517e4b1da64cd8a9a46070347fc
Salah Eddine BENALI <salaheddinebenali@student.emi.ac.ma>
2021-05-03 02:35:11 +0000

    Delete artisanatshop directory

--- artisanatshop/src/main/java/com/itwins/artisanatshop/models/View.java
+++
@@ -1,8 +0,0 @@
-package com.itwins.artisanatshop.models;
-
-
-//This is used only for the purpose to control the data that is shown in the JSON responses.
-public class View {
-    public interface Commandes {}
-    public interface LigneCommande {}
-}
